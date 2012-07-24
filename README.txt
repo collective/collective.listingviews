59a559ec45c977af7bcf2c1049b1ed7f37dd63fa
Ivan Teoh <ivan@pretaweb.com>
2012-07-24 14:50:26 +1000

    Init project generated by templer package.

---
+++ README.txt
@@ -0,0 +1,6 @@
+.. contents::
+
+Introduction
+============
+
+Listing views package
---
+++ src/collective/listingviews/testing.py
@@ -0,0 +1,27 @@
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import applyProfile
+
+from zope.configuration import xmlconfig
+
+class CollectiveListingviews(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML for this package
+        import collective.listingviews
+        xmlconfig.file('configure.zcml',
+                       collective.listingviews,
+                       context=configurationContext)
+
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'collective.listingviews:default')
+
+COLLECTIVE_LISTINGVIEWS_FIXTURE = CollectiveListingviews()
+COLLECTIVE_LISTINGVIEWS_INTEGRATION_TESTING = \
+    IntegrationTesting(bases=(COLLECTIVE_LISTINGVIEWS_FIXTURE, ),
+                       name="CollectiveListingviews:Integration")
