55bb1d88e0407b00fc9aa38b4f6ddca7a21dda4a
David Bain <pigeonflight@gmail.com>
2015-08-30 21:22:28 -0500

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -16,7 +16,6 @@
 Simply run this script in a directory containing a buildout.cfg.
 The script accepts buildout command-line options, so you can
 use the -c option to specify an alternate configuration file.
-just testing travis
 """
 
 import os, shutil, sys, tempfile, urllib, urllib2, subprocess
