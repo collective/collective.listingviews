203b6baace16acb6a949a6e80f8ce77e22b3ced0
Jon Pentland <jon@iomedia.co.uk>
2019-01-16 16:36:48 +0000

    Plone5 Test fixes

--- src/collective/listingviews/tests.py
+++ src/collective/listingviews/tests.py
@@ -3,10 +3,9 @@ import doctest
 from collective.listingviews.testing import\
     COLLECTIVE_LISTINGVIEWS_INTEGRATION_TESTING
 
-
-
+from collective.listingviews import plone_version
+plone5 = plone_version >= "5"
 OPTIONFLAGS = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE | doctest.REPORT_NDIFF | doctest.REPORT_ONLY_FIRST_FAILURE
-
 
 
 def test_suite():
@@ -17,7 +16,7 @@ def test_suite():
 #    seltest.level = 2
     layer = COLLECTIVE_LISTINGVIEWS_INTEGRATION_TESTING
     suite.addTests([
-        doctest.DocFileSuite('tests/listingviews.rst', optionflags=OPTIONFLAGS, globs=dict(layer=layer)),
+        doctest.DocFileSuite('tests/listingviews.rst', optionflags=OPTIONFLAGS, globs=dict(layer=layer, plone5=plone5)),
         doctest.DocFileSuite('tests/listingviews_sortable_collections.rst', optionflags=OPTIONFLAGS, globs=dict(layer=layer)),
     ])
     suite.layer = COLLECTIVE_LISTINGVIEWS_INTEGRATION_TESTING
