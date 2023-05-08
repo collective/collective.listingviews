e9f18d27c794322c693927f1ae1fe1e84410721a
Dylan Jay <software@pretaweb.com>
2023-05-08 16:56:57 +0700

    add actions support

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
-[ ! -f bin/pip ] && virtualenv .
-bin/pip install --upgrade pip setuptools zc.buildout
-bin/buildout -c $1 annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
+[ ! -f venv/bin/pip ] && virtualenv-2.7 venv
+venv/bin/pip install --upgrade pip setuptools zc.buildout
+venv/bin/buildout -c $1 annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
 cat annotate.txt
 cat requirements.txt
-bin/pip install --upgrade -r requirements.txt
+venv/bin/pip install --upgrade -r requirements.txt
